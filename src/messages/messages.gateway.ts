25137701b7a1f8904099c75e0d6579495f7802c3
Sumeet213 <desaisumeet02@gmail.com>
2022-06-21 15:49:53 +0530

    Compelted socket connection

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,7 +1,8 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer, ConnectedSocket } from '@nestjs/websockets';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import{Server,Socket} from 'socket.io';
 
 
 
@@ -11,11 +12,21 @@ import { UpdateMessageDto } from './dto/update-message.dto';
   },
 })
 export class MessagesGateway {
+  @WebSocketServer()
+  server: Server;
+  
   constructor(private readonly messagesService: MessagesService) {}
 
   @SubscribeMessage('createMessage')
-  create(@MessageBody() createMessageDto: CreateMessageDto) {
-    return this.messagesService.create(createMessageDto);
+  async create(@MessageBody() createMessageDto: CreateMessageDto,@ConnectedSocket() client:Socket) {
+    const message = await this.messagesService.create(
+      createMessageDto, 
+      client.id,
+      );
+    
+    this.server.emit('message',message);
+    
+    return message;
   }
 
   @SubscribeMessage('findAllMessages')
@@ -23,7 +34,28 @@ export class MessagesGateway {
     return this.messagesService.findAll();
   }
 
+  @SubscribeMessage('join')
+  joinRoom(
+    @MessageBody('name') name:string,
+    @ConnectedSocket()client:Socket)
+    {
+      return this.messagesService.identify(name,client.id);
 
+  }
+
+  @SubscribeMessage('typing')
+  async typing(
+    @MessageBody('isTyping')isTyping:boolean,
+    @ConnectedSocket()client:Socket,
+    ){
+
+      const name = await this.messagesService.getClientName(client.id);
+
+      client.broadcast.emit('typing',{name,isTyping});
+      
+   
+
+  }
 
  
 }
